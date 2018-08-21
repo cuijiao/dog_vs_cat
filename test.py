f9f07ae56567b0bcd766dc06a45fa16bce35c745
jiao.cui <jiao.cui@rea-group.com>
2018-08-21 09:16:22 +0800

    init repo

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2                 # working with, mainly resizing, images
+import numpy as np         # dealing with arrays
+import os                  # dealing with directories
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm
+
+TRAIN_DIR = '/Users/jcui/MachineLearning/all/train'
+TEST_DIR = '/Users/jcui/MachineLearning/all/test'
+IMG_SIZE = 50
+LR = 1e-3
+
+MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    if word_label == 'cat': return [1,0]
+    elif word_label == 'dog': return [0,1]
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+create_train_data()
